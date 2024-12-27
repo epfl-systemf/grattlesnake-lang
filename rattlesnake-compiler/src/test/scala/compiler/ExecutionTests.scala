57025ab648c258646e5d11c88094fa303f7ecc6f
Valentin Aebi <valentin.aebi@epfl.ch>
2024-12-27 22:58:15 +0100

    Add support for test programs that are expected to crash

--- rattlesnake-compiler/src/test/scala/compiler/ExecutionTests.scala
+++ rattlesnake-compiler/src/test/scala/compiler/ExecutionTests.scala
@@ -1,13 +1,12 @@
 package compiler
 
-import compiler.ExecutionTests.{rootPathStr, stderrFileName, stdoutFileName, testOutSubdirName}
-import compiler.gennames.ClassesAndDirectoriesNames.outDirName
-import compiler.gennames.{ClassesAndDirectoriesNames, FileExtensions}
+import compiler.ExecutionTests.*
+import compiler.gennames.FileExtensions
 import compiler.io.SourceFile
 import compiler.pipeline.TasksPipelines
 import compiler.reporting.Errors.ErrorReporter
 import compiler.runners.{MainFinder, Runner}
-import org.junit.Assert.assertEquals
+import org.junit.Assert.{assertEquals, assertNotEquals, assertTrue}
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.runners.Parameterized
@@ -17,6 +16,7 @@ import org.objectweb.asm.Opcodes.V1_8
 import java.io.InputStream
 import java.nio.file.{Files, Path, Paths}
 import scala.io.Source
+import scala.util.Using
 
 
 object ExecutionTests {
@@ -24,7 +24,9 @@ object ExecutionTests {
   private val rootPathStr: String = "./src/test/res/execution-tests/"
   private val stdoutFileName: String = "stdout.exp"
   private val stderrFileName: String = "stderr.exp"
+  private val stacktraceFileName: String = "error-trace.exp"
   private val testOutSubdirName: String = "testout"
+  private val stackTraceCommentMarker = "//>STACKTRACE"
   private val javaVersionCode = V1_8
 
   @Parameters(name = "{0}")
@@ -36,12 +38,14 @@ object ExecutionTests {
 
 @RunWith(classOf[Parameterized])
 class ExecutionTests(programDirName: String) {
+
   import ExecutionTests.javaVersionCode
-  
+
+  private val programDirPath = Path.of(rootPathStr).resolve(programDirName)
+
   @Test
   def runProgramTest(): Unit = {
 
-    val programDirPath = Path.of(rootPathStr).resolve(programDirName)
     val expOut = getExpectedData(programDirPath.resolve(stdoutFileName))
 
     object ExitException extends RuntimeException
@@ -49,9 +53,10 @@ class ExecutionTests(programDirName: String) {
     val er = ErrorReporter(System.err.println, exit = throw ExitException)
     val srcFiles = getAllSourcesInProgram(programDirPath).map(s => SourceFile(programDirPath.resolve(s).toString))
     val testOutSubdirPath = programDirPath.resolve(testOutSubdirName)
-    val writtenFilePaths = TasksPipelines
-      .compiler(testOutSubdirPath, javaVersionCode, er)
-      .apply(srcFiles)
+    val writtenFilePaths =
+      TasksPipelines
+        .compiler(testOutSubdirPath, javaVersionCode, er)
+        .apply(srcFiles)
 
     def errorCallback(msg: String): Nothing = {
       throw AssertionError(msg)
@@ -64,8 +69,20 @@ class ExecutionTests(programDirName: String) {
     val actOut = readProgramStream(process.getInputStream)
     val actErr = readProgramStream(process.getErrorStream)
 
-    assertEquals(s"unexpected output on stdout (exit code: $exitCode, stderr:\n$actErr\n)", expOut, actOut)
-    assertEquals(s"non-zero exit code, stderr: $actErr", 0, exitCode)
+    val msgOpt = maybeGetTraceMessage()
+    val stacktraceEntries = getTraceEntries(srcFiles)
+    assert(!(msgOpt.isDefined && stacktraceEntries.isEmpty),
+      "no stacktrace entry provided for a test program that is expected to crash")
+    assert(!(msgOpt.isEmpty && stacktraceEntries.nonEmpty),
+      "test expects some lines to appear in a stacktrace but does not specify the error message")
+
+    if (msgOpt.isDefined) {
+      assertMatches(msgOpt.get, stacktraceEntries, actErr)
+      assertNotEquals("expected an error, but got a zero exit code", 0, exitCode)
+    } else {
+      assertEquals(s"unexpected output on stdout (exit code: $exitCode)", expOut, actOut)
+      assertEquals(s"non-zero exit code", 0, exitCode)
+    }
   }
 
   private def readProgramStream(is: InputStream): String = {
@@ -82,5 +99,62 @@ class ExecutionTests(programDirName: String) {
   private def getAllSourcesInProgram(dirPath: Path): List[String] = {
     dirPath.toFile.list((_, name) => name.endsWith(FileExtensions.rattlesnake)).toList
   }
-  
+
+  private def maybeGetTraceMessage(): Option[String] = {
+    val traceFile = programDirPath.resolve(stacktraceFileName).toFile
+    if (traceFile.exists()) {
+      val lines = Using(Source.fromFile(traceFile))(_.getLines().mkString("\n")).get
+      Some(lines)
+    } else None
+  }
+
+  private def getTraceEntries(srcFiles: Seq[SourceFile]): Seq[(Filename, Int)] = {
+    srcFiles.flatMap(getTraceEntries)
+  }
+
+  private def getTraceEntries(srcFile: SourceFile): Seq[(Filename, Int)] = {
+    val srcFileName = nameFromPath(srcFile.name)
+    val lines = srcFile.lines.get
+    val entriesB = Seq.newBuilder[(Filename, Int)]
+    var lineIdx = 1
+    for line <- lines do {
+      if (line.contains(stackTraceCommentMarker)) {
+        entriesB.addOne((srcFileName, lineIdx))
+      }
+      lineIdx += 1
+    }
+    entriesB.result()
+  }
+
+  private type Filename = String
+
+  private def assertMatches(expMsg: String, reqEntries: Seq[(Filename, Int)], actErr: String): Unit = {
+    val lines = actErr.lines().toArray(new Array[String](_))
+    val actMsg = lines.head.split(':')(1)
+    val actualEntries = extractEntriesFromStacktrace(lines.tail)
+    assertEquals(expMsg.trim, actMsg.trim)
+    for (fn, l) <- reqEntries do {
+      assertTrue(s"expected entry not found: $fn:$l",
+        actualEntries.exists((afn, alo) => afn == fn && alo.contains(l)))
+    }
+  }
+
+  private def extractEntriesFromStacktrace(lines: Array[String]): Array[(Filename, Option[Int])] = {
+    lines.map { line =>
+      val openParenthIdx = line.indexOf('(')
+      val idxOfLastSep = line.lastIndexOf('/') max line.lastIndexOf('\\')
+      val startIdx = (openParenthIdx max idxOfLastSep) + 1
+      val fileAndLine = line.substring(startIdx, line.length - 1)
+      fileAndLine.split(':') match {
+        case Array(actualFileName) =>
+          (nameFromPath(actualFileName), None)
+        case Array(actualFileName, lineIdxStr) =>
+          (nameFromPath(actualFileName), Some(lineIdxStr.toInt))
+        case _ => assert(false)
+      }
+    }
+  }
+
+  private def nameFromPath(p: String): String = Path.of(p).getFileName.toString
+
 }
