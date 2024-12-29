45b39b454d6ba4f08da1056fadbf5b83d08d758f
Valentin Aebi <valentin.aebi@epfl.ch>
2024-12-29 19:00:08 +0100

    Allow tail-call elimination also in Void functions

--- rattlesnake-compiler/src/main/scala/compiler/tailrecchecker/TailrecChecker.scala
+++ rattlesnake-compiler/src/main/scala/compiler/tailrecchecker/TailrecChecker.scala
@@ -5,7 +5,7 @@ import compiler.irs.Asts
 import compiler.irs.Asts.*
 import compiler.pipeline.CompilationStep.TailrecChecking
 import compiler.pipeline.CompilerStep
-import compiler.reporting.Errors.{Err, ErrorReporter}
+import compiler.reporting.Errors.{Err, ErrorReporter, Warning}
 
 final class TailrecChecker(errorReporter: ErrorReporter)
   extends CompilerStep[(List[Source], AnalysisContext), (List[Source], AnalysisContext)] {
@@ -21,6 +21,16 @@ final class TailrecChecker(errorReporter: ErrorReporter)
 
   private def check(ast: Ast, isTailPosition: Boolean): Unit = {
     ast match {
+      case funDef: FunDef =>
+        check(funDef.body, true)
+      case Block(stats) if stats.size >= 2 =>
+        stats.init.foreach(check(_, false))
+        check(stats.last, isTailPosition)
+      case Block(stats) =>
+        stats.headOption.foreach(check(_, isTailPosition))
+      case Sequence(stats, expr) =>
+        stats.foreach(check(_, false))
+        check(expr, isTailPosition)
       case Call(receiverOpt, funName, args, isTailrec) if isTailrec && !isTailPosition =>
         errorReporter.push(Err(TailrecChecking, s"call to '$funName' is not in tail position", ast.getPosition))
         args.foreach(check(_, false))
@@ -30,6 +40,18 @@ final class TailrecChecker(errorReporter: ErrorReporter)
         check(elseBr, isTailPosition)
       case ReturnStat(Some(expr)) =>
         check(expr, true)
+      case retStat@ReturnStat(None) if isTailPosition =>
+        errorReporter.push(Warning(TailrecChecking, "unnecessary return", retStat.getPosition))
+      case IfThenElse(cond, thenBr, elseBrOpt) =>
+        check(cond, false)
+        check(thenBr, isTailPosition)
+        elseBrOpt.foreach(check(_, isTailPosition))
+      case RestrictedStat(captureSet, body) =>
+        check(captureSet, false)
+        check(body, isTailPosition)
+      case EnclosedStat(captureSet, body) =>
+        check(captureSet, false)
+        check(body, isTailPosition)
       case _ =>
         ast.children.foreach(check(_, false))
     }
