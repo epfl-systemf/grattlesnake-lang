e9b3309187cda0cf31d2e97949bec6a69b0f697e
Valentin Aebi <valentin.aebi@epfl.ch>
2025-01-08 09:27:35 +0100

    Remove check against impure returns from enclosure

--- rattlesnake-compiler/src/main/scala/compiler/typechecker/TypeChecker.scala
+++ rattlesnake-compiler/src/main/scala/compiler/typechecker/TypeChecker.scala
@@ -512,9 +512,6 @@ final class TypeChecker(errorReporter: ErrorReporter)
       valueOpt.foreach { value =>
         val retType = checkExpr(value)
         checkSubtypingConstraint(expRetType, retType, retStat.getPosition, "returned value")
-        if (tcCtx.insideEnclosure && !retType.isPure) {
-          reportError("cannot return a value of an impure type from inside an enclosure", retStat.getPosition)
-        }
       }
       if (expRetType == NothingType) {
         reportError(s"unexpected return in function returning $NothingType", retStat.getPosition)
